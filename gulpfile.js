4882c1376cd6f3d163c668d75da064b793fe4e52
野猪yz <1746534101@qq.com>
2016-05-06 14:01:57 +0800

    0.0.3

--- gulpfile.js
+++ gulpfile.js
@@ -94,6 +94,7 @@ gulp.task('hb.jquery', function () {
 
 gulp.task('hb.angular.pack', function () {
     var timestamp=new Date().getTime();
+    var vision='1.0.0';
     var banner = `/**
      * timestamp: ${timestamp}
      * 1.5.5 angular,angular-animate,angular-messages,angular-resource,angular-sanitize,angular-touch
@@ -108,20 +109,54 @@ gulp.task('hb.angular.pack', function () {
         'node_modules/angular-resource/angular-resource.js',
         'node_modules/angular-sanitize/angular-sanitize.js',
         'node_modules/angular-touch/angular-touch.js',
-        'node_modules/angular-ui-bootstrap/dist/ui-bootstrap.tpl.js',
+        'node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js',
         'node_modules/angular-ui-router/release/angular-ui-router.js',
     ])
         //.pipe(sourcemaps.init())
-        .pipe(plugins.concat('hb.angular.pack.js'))
+        .pipe(plugins.concat(`hb.angular.pack.${vision}.js`))
         .pipe(gulp.dest('dist'))
         .pipe(plugins.uglify())
         .pipe(plugins.header(banner))
-        .pipe(plugins.rename("hb.angular.pack.min.js"))
+        .pipe(plugins.rename(`hb.angular.pack.min.${vision}.js`))
         //.pipe(sourcemaps.write('../maps/less'))
         .pipe(gulp.dest('dist'));
 });
+gulp.task('hb.jquery.angular.pack', function () {
+    var timestamp=new Date().getTime();
+    var vision='1.0.0';
+    var banner = `/**
+     * timestamp: ${timestamp}
+     * 2.2.3 jquery
+     * 1.5.5 angular,angular-animate,angular-messages,angular-resource,angular-sanitize,angular-touch
+     * 0.2.18 angular-ui-router
+     * 1.3.2 ui-bootstrap.tpl
+     */\n`;
+    return gulp.src([
+        'node_modules/jquery/dist/jquery.js',
+        'node_modules/angular/angular.js',
+        'node_modules/angular-animate/angular-animate.js',
+        'node_modules/angular-messages/angular-messages.js',
+        'node_modules/angular-resource/angular-resource.js',
+        'node_modules/angular-sanitize/angular-sanitize.js',
+        'node_modules/angular-touch/angular-touch.js',
+        'node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js',
+        'node_modules/angular-ui-router/release/angular-ui-router.js',
+    ])
+        //.pipe(sourcemaps.init())
+        .pipe(plugins.concat(`hb.jquery.angular.pack.${vision}.js`))
+        .pipe(gulp.dest('dist'))
+        .pipe(plugins.uglify())
+        .pipe(plugins.header(banner))
+        .pipe(plugins.rename(`hb.jquery.angular.pack.min.${vision}.js`))
+        //.pipe(sourcemaps.write('../maps/less'))
+        .pipe(gulp.dest('dist'));
+});
+
+
+
+
 gulp.task('lib', function(){
-    gulp.start(['hb.angular.pack']);
+    gulp.start(['hb.angular.pack','hb.jquery.angular.pack']);
 });
 
 
