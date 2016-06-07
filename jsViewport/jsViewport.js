190bc72493bf40a1ca500659425d536dcf072686
野猪yz <1746534101@qq.com>
2016-06-07 18:40:29 +0800

    0.0.4

--- jsViewport/jsViewport.js
+++ jsViewport/jsViewport.js
@@ -3,20 +3,59 @@
 //            alert(navigator.userAgent);
 //            alert(window.innerWidth);
     //
-    function setViewPort(dw){
+    function setViewPort(mw,mh,tw,th){
         var metaTag=document.createElement('meta');
         metaTag.name = "viewport";
         metaTag.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0";
         document.write(metaTag.outerHTML);
         //
-//            alert(window.innerWidth);
+        //    alert(window.innerWidth);
         //
-        var deviceWidth=window.innerWidth;
-        var scale=deviceWidth/dw;
+
+        var deviceWidth=document.documentElement.clientWidth;
+        var deviceHeight=document.documentElement.clientHeight;
+
+        var scale=1;
+        calScale();
+
+        function calScale(){
+            deviceWidth=document.documentElement.clientWidth;
+            deviceHeight=document.documentElement.clientHeight;
+            if(deviceWidth<deviceHeight){
+                if(deviceWidth<500){
+                    scale=deviceWidth/mw;
+                }else{
+                    scale=deviceWidth/tw;
+                }
+            }else{
+                if(deviceWidth<1000){
+                    scale=deviceWidth/mh;
+                }else{
+                    scale=deviceWidth/th;
+                }
+            }
+        }
+
+
+
+
+
 
 
         var viewport = document.querySelector("meta[name=viewport]");
         viewport.setAttribute('content', 'width=device-width, initial-scale='+scale+', maximum-scale='+scale+', user-scalable=0');
+
+
+
+        window.addEventListener("orientationchange", function(event){
+            //alert(event.orientation);
+
+            viewport.setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0');
+            deviceWidth=document.documentElement.clientWidth;
+            calScale();
+            viewport.setAttribute('content', 'width=device-width, initial-scale='+scale+', maximum-scale='+scale+', user-scalable=0');
+
+        });
 
         document.documentElement.style.fontSize='1px';
 
@@ -25,5 +64,5 @@
 //
 //            alert(document.documentElement.clientWidth);
     }
-    setViewPort(750);
+    setViewPort(750,1334,1536,2048);
 }());
