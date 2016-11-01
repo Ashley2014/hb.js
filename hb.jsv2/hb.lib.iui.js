d0b4d272ae910848d3b8d6ce03e30bb043db2cb8
野猪yz <1746534101@qq.com>
2016-11-01 10:06:29 +0800

    iui

--- hb.jsv2/hb.lib.iui.js
+++ hb.jsv2/hb.lib.iui.js
@@ -1,4 +1,120 @@
+;(function(window) {
 
+    var svgSprite = '<svg>' +
+        ''+
+        '<symbol id="icon-success" viewBox="0 0 1024 1024">'+
+        ''+
+        '<path d="M83.2 550.4l89.6-70.4L384 684.8c0 0 275.2-339.2 556.8-531.2l25.6 64c-134.4 96-384 390.4-512 684.8L83.2 550.4z"  ></path>'+
+        ''+
+        '</symbol>'+
+        ''+
+        '</svg>'
+    var script = function() {
+        var scripts = document.getElementsByTagName('script')
+        return scripts[scripts.length - 1]
+    }()
+    var shouldInjectCss = script.getAttribute("data-injectcss")
+
+    /**
+     * document ready
+     */
+    var ready = function(fn){
+        if(document.addEventListener){
+            document.addEventListener("DOMContentLoaded",function myFun(){
+                document.removeEventListener("DOMContentLoaded",myFun,false)
+                fn()
+            },false)
+        }else if(document.attachEvent){
+            IEContentLoaded (window, fn)
+        }
+
+        function IEContentLoaded (w, fn) {
+            var d = w.document, done = false,
+            // only fire once
+                init = function () {
+                    if (!done) {
+                        done = true
+                        fn()
+                    }
+                }
+            // polling for no errors
+                ;(function myFun() {
+                try {
+                    // throws errors until after ondocumentready
+                    d.documentElement.doScroll('left')
+                } catch (e) {
+                    setTimeout(myFun, 50)
+                    return
+                }
+                // no errors, fire
+
+                init()
+            })()
+            // trying to always fire before onload
+            d.onreadystatechange = function() {
+                if (d.readyState == 'complete') {
+                    d.onreadystatechange = null
+                    init()
+                }
+            }
+        }
+    }
+
+    /**
+     * Insert el before target
+     *
+     * @param {Element} el
+     * @param {Element} target
+     */
+
+    var before = function (el, target) {
+        target.parentNode.insertBefore(el, target)
+    }
+
+    /**
+     * Prepend el to target
+     *
+     * @param {Element} el
+     * @param {Element} target
+     */
+
+    var prepend = function (el, target) {
+        if (target.firstChild) {
+            before(el, target.firstChild)
+        } else {
+            target.appendChild(el)
+        }
+    }
+
+    function appendSvg(){
+        var div,svg
+
+        div = document.createElement('div')
+        div.innerHTML = svgSprite
+        svg = div.getElementsByTagName('svg')[0]
+        if (svg) {
+            svg.setAttribute('aria-hidden', 'true')
+            svg.style.position = 'absolute'
+            svg.style.width = 0
+            svg.style.height = 0
+            svg.style.overflow = 'hidden'
+            prepend(svg,document.body)
+        }
+    }
+
+    if(shouldInjectCss && !window.__iconfont__svg__cssinject__){
+        window.__iconfont__svg__cssinject__ = true
+        try{
+            document.write("<style>.svgfont {display: inline-block;width: 1em;height: 1em;fill: currentColor;vertical-align: -0.1em;font-size:16px;}</style>");
+        }catch(e){
+            console && console.log(e)
+        }
+    }
+
+    ready(appendSvg)
+
+
+})(window)
 import styles from "./style.scss";
 //confirm('aaa')
 console.log(styles)
@@ -134,7 +250,9 @@ var iui=(function(){
         var toastHtmlStr=`<div >
 <div class="${styles['weui-mask_transparent']}"></div>
 <div class="${styles['weui-toast']}">
-    <i class="${styles['weui-icon-success-no-circle']} ${styles['weui-icon_toast']} glyphicon glyphicon-ok"></i>
+    <svg class="${styles['icon']}" aria-hidden="true" style="font-size: 80px;">
+      <use xlink:href="#icon-success"></use>
+    </svg>
     <p class="${styles['weui-toast__content']}">${msg}</p>
 </div>
 </div>`;
@@ -145,7 +263,7 @@ var iui=(function(){
         var hideToast=function(){
             $toastHtml.fadeOut(400).remove();
         };
-        //setTimeout(hideToast,600);
+        setTimeout(hideToast,600);
     };
 
 
